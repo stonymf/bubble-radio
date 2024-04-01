78a679bdf9a742ee863b8fe28103f0f4691ee60c
stonymf <tonymark@fastmail.com>
2024-04-01 19:11:22 +0000

    add emoji argument so that when u call script u supply the emoji_name as 2nd argument

--- src/test_post_request.py
+++ src/test_post_request.py
@@ -10,20 +10,22 @@ flask_port = os.getenv("FLASK_PORT")
 secret_key = os.getenv("SECRET_KEY")
 
 # Check for the minimum number of arguments
-if len(sys.argv) < 2:
-    song_url = "https://www.youtube.com/watch?v=KOFw2UPLdPk"
-    #print("Usage: python test_post_request.py <url> [timestamp]")
-    #sys.exit(1)
+if len(sys.argv) < 3:
+    print("Usage: python test_post_request.py <url> <emoji_name> [timestamp]")
+    sys.exit(1)
 
 # First command-line argument is the URL
 song_url = sys.argv[1]
 
+# Second command-line argument is the emoji_name
+emoji_name = sys.argv[2]
+
 # Use the current time as the default timestamp
 timestamp = datetime.now().isoformat()
 
-# If a second argument is provided, use it as the timestamp
-if len(sys.argv) > 2:
-    timestamp = sys.argv[2]
+# If a third argument is provided, use it as the timestamp
+if len(sys.argv) > 3:
+    timestamp = sys.argv[3]
 
 url = f"http://localhost:{flask_port}/add_song"
 headers = {
@@ -34,10 +36,10 @@ data = {
     "url": song_url,
     "user": "test_user",
     "timestamp": timestamp,
-    "channel_id": 987654321,
-    "server_id": 123456789,
-    "emoji_name": "chao1",
-    "emoji_id": 123456789
+    "channel_id": 987654321,  # Assuming this is still needed
+    "server_id": 123456789,   # Assuming this is still needed
+    "emoji_name": emoji_name,
+    "emoji_id": 123456789     # Assuming a default value or this should be provided as another argument
 }
 
 response = requests.post(url, headers=headers, data=json.dumps(data))
